ad9a6344c18f6e1cc73d16d3aa545a01e48aac71
Raydelx <rat.esp.kone@gmail.com>
2025-07-30 11:43:54 +0200

    last product upodated and form index translated

--- src/hooks/SliderReactHome.jsx
+++ src/hooks/SliderReactHome.jsx
@@ -7,7 +7,6 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 // import required modules
-
 import {Autoplay, Pagination, Navigation } from 'swiper/modules';
 import './style.css';
 
